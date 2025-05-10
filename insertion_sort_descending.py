5fa0b9a5070fa95ee529a0e29bb36d9923a4c246
sushil34053 <skhanal34053@ucumberlands.edu>
2025-05-10 15:42:56 +0300

    Update insertion_sort_descending.py to include comments
    This is the second commit

--- insertion_sort_descending.py
+++ insertion_sort_descending.py
@@ -2,6 +2,7 @@ def insertion_sort_descending(arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1        
+        # Move elements that are smaller than key forward
         while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
             j -= 1
